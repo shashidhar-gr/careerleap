017d66bcd1f47b3aed0f26c6d760735cd3cbd78f
shashidhar <shashidhargr26@gmail.com>
2023-06-07 13:06:01 +0530

    Finished moving zeros to end

--- Assignment5/MoveZerosToEnd.js
+++ Assignment5/MoveZerosToEnd.js
@@ -1,9 +1,31 @@
 /**
+ * 283. Move Zeroes
+ * https://leetcode.com/problems/move-zeroes/description/
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-// Bruteforce solution.
+//TC: O(N)
 var moveZeroes = function(nums) {
+    let reader = 0, writer = 0;
+
+    while(nums[writer] != 0 && writer < nums.length) {
+        writer++;
+    }
+
+    for(reader = writer; reader < nums.length; reader++) {
+        if(nums[reader] != 0) {
+            let temp = nums[writer];
+            nums[writer] = nums[reader];
+            nums[reader] = temp;
+            writer++;
+        }
+    }
+    
+    return nums;
+}
+
+// Bruteforce solution. TC: O(N^2)
+/*var moveZeroes = function(nums) {
     let i = 0; let pivot = nums.length-1;
     while(i < nums.length-1) {
         if(nums[i] == 0 && i <= pivot) {
@@ -20,7 +42,7 @@ var moveZeroes = function(nums) {
         }
     }
     return nums;
-};
+}; */
 
-const arr = [0,1,0,3,12];
+const arr = [1];
 console.log(moveZeroes(arr));
