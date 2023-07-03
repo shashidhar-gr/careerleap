b45b546d421e0423e72b378a935c71e072fc3682
shashidhar <shashidhargr26@gmail.com>
2023-07-03 21:41:25 +0530

    Finished Maze problem

--- Assignment17/MazeSolver.js
+++ Assignment17/MazeSolver.js
@@ -1,52 +1,33 @@
 var solver = function(arr) {
-    let search = function(arr, row, col, path) {
-        if((row == arr.length - 1 && col == arr[0].length - 1)) {
-            path.push([row, col]);
+    let search = function(arr, row, col, i, j, visited) {
+        if((row == i && col == j)) {
+            visited[row][col] = 1;
             return true;
         }
         
-        if((col > arr[0].length - 1 || col < 0)
-        || (row > arr.length - 1|| row < 0))
+        if((col > j || col < 0)
+        || (row > i || row < 0) || arr[row][col] == 0 || visited[row][col] == 1)
             return false;
 
-        path.push([row, col]);
+        visited[row][col] = 1;
 
-        if(arr[row][col] == 0)
-            return false;
-
-        let moveright = search(arr, row, col + 1, path);
-
-        if(!moveright)
-            path.pop();
-        else 
+        if(search(arr, row, col + 1, i, j, visited) || search(arr, row + 1, col, i, j, visited)
+        || search(arr, row, col - 1, i, j, visited) || search(arr, row - 1, col, i, j, visited)) {
             return true;
+        }
 
-        let movedown = search(arr, row + 1, col, path);
-
-        if(!movedown)
-            path.pop();
-        else 
-            return true;
-
-        let moveleft = search(arr, row, col - 1, path);
-
-        if(!moveleft)
-            path.pop();
-        else 
-            return true;
-
-        let moveup = search(arr, row - 1, col, path);
-
-        if(!moveup)
-            path.pop();
-        else 
-            return true;
-
-        return true;
+        visited[row][col] = 0;
     }
-    let path = [];
-    search(arr, 0, 0, path);
-    console.log(path);
+
+    let visited = new Array(arr.length);
+    let colsize = arr[0].length;
+    for(let i = 0; i < arr.length; i++) {
+        let arrcol = new Array(colsize).fill(0);
+        visited[i] = arrcol;
+    }
+
+    search(arr, 0, 0, 3, 3, visited);
+    console.log(visited);
 }
 
 const arr = [[1, 1, 1, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 0, 1, 1]];
